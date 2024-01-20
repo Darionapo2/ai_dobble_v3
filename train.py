390d58d8156b509477937ecca7610cc0a00dbd81
Dario <darionapo2.0@gmail.com>
2024-01-20 19:59:43 +0100

    some updates

--- train.py
+++ train.py
@@ -13,22 +13,54 @@ nrows = 240
 ncols = 320
 nchannels = 3
 
-card_decks = [
-    'exp0-augmented2'
-]
+card_decks = ['exp0-augmented2']
 
 nb_card_decks = len(card_decks)
 
+
+def main():
+    train_cards = prepare_train_cards(card_decks)
+    X, y, n_train = build_dataset(train_cards)
+
+    print('Shape of X:', X.shape)
+    print('Shape of y:', y.shape)
+    print('Training dataset overall number of samples', n_train)
+
+    seed = 3
+    test_size = 0.20
+
+    train_X, val_X, train_y, val_y = train_test_split(
+        X, y, test_size = test_size, random_state = seed
+    )
+
+    print('Shape of training data (train_X):', train_X.shape)
+    print('Shape of training data (train_y):', train_y.shape)
+    print('Shape of validation data (val_X):', val_X.shape)
+    print('Shape of validation data (val_y):', val_y.shape)
+
+    # convert labels in range 0-29 to one-hot encoding
+    train_y = to_categorical(train_y, 30)
+    val_y = to_categorical(val_y, 30)
+
+    model = create_model()
+
+    entrenador_automatico(
+        model, train_X, train_y, val_X, val_y,
+        batch_size = 16, nepochs = 10
+    )
+
+
 # Capture images/labels from data set for training and testing
-def prepare_train_cards(card_decks):
+def prepare_train_cards(decks):
     train_cards = []
-    for deck in card_decks:
+    for deck in decks:
         train_dir = f'{dataset_dir}/{deck}'
         filenames = db.get_card_filenames(train_dir)
         train_cards.append(filenames)
 
     gc.collect()
     return train_cards
+
 
 # Read images and pre-process to fixed size
 def build_dataset(train_cards):
@@ -50,18 +82,14 @@ def build_dataset(train_cards):
     return all_X, all_y, n_train
 
 
-train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size = 0.20,
-                                                  random_state = 2)
-print('TRAINING/VALIDATION DATA SETS:')
-print('Shape of training data (X) is:', train_X.shape)
-print('Shape of training data (y) is:', train_y.shape)
-print('Shape of validation data (X) is:', val_X.shape)
-print('Shape of validation data (y) is:', val_y.shape)
-
 def create_model():
     model = models.Sequential()
-    model.add(layers.Conv2D(filters = 32, kernel_size = (3, 3), activation = 'relu',
-                            input_shape = (nrows, ncols, nchannels)))
+
+    model.add(layers.Conv2D(
+        filters = 32, kernel_size = (3, 3),
+        activation = 'relu', input_shape = (nrows, ncols, nchannels))
+
+    )
     model.add(layers.MaxPooling2D(pool_size = (2, 2)))
 
     model.add(layers.Conv2D(filters = 64, kernel_size = (3, 3), activation = 'relu'))
@@ -85,30 +113,26 @@ def create_model():
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam')
     model.summary()
 
+    return model
 
-def entrenador_automatico(model, train_X, train_y, val_X, val_y):
+
+def entrenador_automatico(model, train_X, train_y, val_X, val_y, batch_size, nepochs):
     ntrain = len(train_X)
     nval = len(val_X)
-    batch_size = 16
-    nepochs = 10
-
-    # convert labels in range 0-29 to one-hot encoding
-    train_y = to_categorical(train_y, 30)
-    val_y = to_categorical(val_y, 30)
 
     train_datagen = ImageDataGenerator(
-        rescale = 1. / 255,
+        rescale = 1.0 / 255,
         rotation_range = 360,
         horizontal_flip = True
     )
 
-    val_datagen = ImageDataGenerator(rescale = 1. / 255)
+    val_datagen = ImageDataGenerator(rescale = 1.0 / 255)
 
     # Definition of useful objects implemented in the training
     train_generator = train_datagen.flow(train_X, train_y, batch_size = batch_size)
     val_generator = val_datagen.flow(val_X, val_y, batch_size = batch_size)
 
-    # The actual model training.
+    # The actual model training
     history = model.fit(
         train_generator,
         steps_per_epoch = int(ntrain / batch_size),
@@ -120,10 +144,7 @@ def entrenador_automatico(model, train_X, train_y, val_X, val_y):
     model.save_weights('dobble_model_weights.h5')
     model.save('dobble_model.h5')
 
-
-def main():
-
-
+    return history
 
 if __name__ == '__main__':
     main()
